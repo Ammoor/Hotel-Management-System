ed664828af13d4858942d216299163f8503aa925
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-05-05 13:51:41 +0300

    Add Room.h

---
+++ Header files/Room.h
@@ -0,0 +1,15 @@
+#pragma once // To make sure that this header file is included only once in the program.
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+class Room
+{
+private:
+	int Room_Number;
+	int Room_Capacity;
+	int Room_Price;		// The price of the room per night.
+	bool Room_Status;  // True if the room is available, and false if it is not.
+	string Room_Type;	// Single, Double, Triple, Quad, Queen, King, Twin, Double-double, Studio, Master Suite, Mini-Suite, Presidential Suite, or Honeymoon Suite.0 
+public:
+
+};
