7c2ce9615d3da49256a46b1ee8c12515c4d2c5f0
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-05-05 13:52:59 +0300

    Create File Organization project.cpp

---
+++ Implementation files/File Organization project.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include "Main_Menu.h"
+#include "Customer.h"
+#include "Room.h"
+using namespace std;
+int main()
+{
+	while (true)
+	{
+		Menu();
+	}
+	return 0;
+}
