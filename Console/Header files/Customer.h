8ef210b0fec9109b68e70960d29292580cc3433c
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-05-10 22:40:50 +0300

    Update Customer.h

--- Console/Header files/Customer.h
+++ Console/Header files/Customer.h
@@ -2,38 +2,30 @@
 #define _CRT_SECURE_NO_DEPRECATE // To avoid the error of using unsafe functions in C++ like (strcpy).
 #include <bits/stdc++.h>
 #include <fstream>
+#define Del_flag '*'
+#define Record_delimiter '|'
 using namespace std;
 class Customer
 {
 private:
+	short header;
 	int ID;
-	char First_name[31];
-	char Middle_name[31];
-	char Last_name[31];
-	char Email[321]; // The maximum length of an email address is 320 characters.
+	char name[60];
 	char Phone_Number[12]; // We assumed that this system is in Egypt.
-	vector<string>Payment_methods;
+	char Payment_method[20];
+	void update_header();
+	short count_records();
 public:
 	Customer();
-	Customer(int ID, char First_name[], char Middle_name[], char Last_name[], char Email[], char Phone_Number[], vector<string>Payment_methods);
+	Customer(int ID, char name[], char Phone_Number[], char Payment_method[]);
 	void set_ID(int ID);
-	void set_First_name(char First_name[]);
-	void set_Middle_name(char Middle_name[]);
-	void set_Last_name(char Last_name[]);
-	void set_Email(char Email[]);
+	void set_name(char name[]);
 	void set_Phone_Number(char Phone_Number[]);
-	void set_Payment_methods(vector<string>Payment_methods);
+	void set_Payment_method(char Payment_method[]);
 	int get_ID();
-	char* get_First_name();
-	char* get_Middle_name();
-	char* get_Last_name();
-	char* get_Email();
+	char* get_name();
 	char* get_Phone_Number();
-	vector<string> get_Payment_methods();
-	void add_Payment_method(string Payment_method);
-	void remove_Payment_method(string Payment_method);
-	void display();
-	void save();
-	void load();
+	char* get_Payment_method();
+	void display_top_five_records();
 	~Customer();
 };
